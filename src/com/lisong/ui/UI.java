00e1abcba0ffc58131bc100b0fb5cc82ddee8cd9
glassy-sky-lisong <http://github.com/glassy-sky-lisong>
2020-05-16 10:33:35 +0800

    four

--- src/com/lisong/ui/UI.java
+++ src/com/lisong/ui/UI.java
@@ -20,35 +20,10 @@ public class UI {
         frame.setTitle("hello,world");
         frame.setSize(600,400);
         frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-        frame.setLocation(400,100);
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
-        JMenuBar jMenuBar = new JMenuBar();
 
-        JMenu fileMenu = new JMenu("文件");
-        JMenu editMenu = new JMenu("编辑");
-        JMenu aboutMenu = new JMenu("关于");
 
-        jMenuBar.add(fileMenu);
-        jMenuBar.add(editMenu);
-        jMenuBar.add(aboutMenu);
 
-        JMenuItem createMi = new JMenuItem("新建");
-        JMenuItem openMi = new JMenuItem("打开");
-        JMenuItem saveMi = new JMenuItem("保存");
-        JMenuItem exitMi = new JMenuItem("退出");
-        fileMenu.add(createMi);
-        fileMenu.add(openMi);
-        fileMenu.add(saveMi);
-        fileMenu.addSeparator();
-        fileMenu.add(exitMi);
-
-        frame.setJMenuBar(jMenuBar);
-
-        createMi.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("新建文件成功");
-            }
-        });
     }
 }
